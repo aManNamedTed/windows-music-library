046c080f528b43334a7836badebf3e46d3ef1358
David Amante <aManNamedTed@users.noreply.github.com>
2018-07-18 10:05:45 -0700

    Create yourFirstSong.cpp

---
+++ yourMusic/yourFirstSong.cpp
@@ -0,0 +1,15 @@
+#include "winmuslib.h"
+
+int main()
+{
+  /* Write your notes here! example: 
+   *
+   * c3(300);
+   *
+   * remember the duration (300) is in milliseconds.
+   *
+   * Also remember that there is no overlapping of beeps, so melodies only. (unless you have multiple Windows machines haha)
+   */
+   
+  return 0;
+}
