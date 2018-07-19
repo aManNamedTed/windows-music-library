0281eeeedf0e121edbec22442d3e1608d5f730a8
David Amante <aManNamedTed@users.noreply.github.com>
2018-07-19 12:52:05 -0700

    Update yourFirstSong.cpp
    added disclaimer about low and high range notes

--- yourMusic/yourFirstSong.cpp
+++ yourMusic/yourFirstSong.cpp
@@ -12,7 +12,11 @@ int main()
    * remember the duration (300) is in milliseconds.
    *
    * Also remember that there is no overlapping of beeps, so melodies only. 
-	 * (unless you have multiple Windows machines haha)
+   * (unless you have multiple Windows machines haha)
+   * 
+   * Finally, remember that notes in the 0 range and 8 range, may not be
+   * heard due to either the nature of your physiology (inability to hear
+   * certain frequencies), or hardware constraints.
    */
    
   return 0;
