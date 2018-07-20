e9502a0e0fc44a6074264e244d9e78f23b48e539
David Amante <aManNamedTed@users.noreply.github.com>
2018-07-19 20:20:20 -0700

    Update yourFirstSong.cpp

--- yourMusic/yourFirstSong.cpp
+++ yourMusic/yourFirstSong.cpp
@@ -14,7 +14,7 @@ int main()
    * Also remember that there is no overlapping of beeps, so melodies only. 
    * (unless you have multiple Windows machines haha)
    * 
-   * Finally, remember that notes in the 0 range and 8 range, may not be
+   * Finally, remember that notes in the 0 range and 8 range may not be
    * heard due to either the nature of your physiology (inability to hear
    * certain frequencies), or hardware constraints.
    */
