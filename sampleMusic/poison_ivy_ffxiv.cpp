6c64a252864b5fb1b685b4e4ea9d55088425640a
aManNamedTed <davidamante17@yahoo.com>
2018-08-02 22:07:03 -0700

    fixed poison ivy third bar? gn4

--- sampleMusic/poison_ivy_ffxiv.cpp
+++ sampleMusic/poison_ivy_ffxiv.cpp
@@ -1,4 +1,4 @@
-// remember to add #include "../src/winmuslib.h" 
+// remember to add #include "../src/winmuslib.h"
 // at the top of your music files for them to work!
 
 #include "../src/winmuslib.h"
@@ -6,14 +6,14 @@
 int main()
 {
   // initializing note duration variables
-  double quarter   = 0; double dottedQuarter = 0; 
-  double whole     = 0; double dottedHalf    = 0; 
-  double half      = 0; double eighth        = 0; 
+  double quarter   = 0; double dottedQuarter = 0;
+  double whole     = 0; double dottedHalf    = 0;
+  double half      = 0; double eighth        = 0;
   double sixteenth = 0;
 
   // change beatsPerMinute to how many bpm you want your song to have
   // example: double beatsPerMinute = 200; [my favorite bpm for this library]
-  double beatsPerMinute = 180; 
+  double beatsPerMinute = 180;
 
   // this sets your note durations based on how many bpm you select
   setNoteDurations(beatsPerMinute, quarter, dottedQuarter, whole, \
@@ -22,7 +22,7 @@ int main()
   // FFXIV OST Saint Mocianne's Arboretum Theme (Poison Ivy)
   fn4(whole); ds4(whole); dn4(half); cn4(half); dn4(half); fn3(dottedHalf);
   rst(half);
-  fn4(whole); ds4(whole); dn4(half); cn4(half); dn4(half); fn3(dottedHalf);
+  fn4(whole); ds4(whole); dn4(half); cn4(half); dn4(half); gn4(dottedHalf);
   fs4(whole); en4(whole); ds4(half); cs4(half); ds4(half); fs3(dottedHalf);
   fs3(quarter); gs3(quarter); an3(dottedHalf); cs4(quarter); en4(dottedQuarter);
   dn4(dottedQuarter); gs3(quarter); cs4(dottedHalf);
